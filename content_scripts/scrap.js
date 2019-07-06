ef85ab5fdc7c9a619f6b934be043c02f0c42fc80
jz4o <26317360+jz4o@users.noreply.github.com>
2019-07-06 09:40:41 +0900

    保存したオプションを取得できるよう実装

--- content_scripts/scrap.js
+++ content_scripts/scrap.js
@@ -1,3 +1,13 @@
+var setting = {
+  matching_url: '',
+  xpath: ''
+}
+
+chrome.storage.local.get(setting, function(items) {
+  setting['matching_url'] = items.matching_url;
+  setting['xpath'] = items.xpath;
+});
+
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  sendResponse(document.title);
+  sendResponse(setting['matching_url'] + "\n" + setting['xpath']);
 });
